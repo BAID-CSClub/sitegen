b71dabc8ab178ba357dca98a10b0807a6deefb46
lihe07 <li@imlihe.com>
2022-08-22 18:29:22 +0800

    Finish routes

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use std::path::Path;
+use std::{path::Path, process::exit};
 
 #[macro_use]
 mod log;
-
 mod articles;
 mod git;
+mod routes;
 
 fn main() {
     let cmd = clap::Command::new("sitegen")
@@ -32,7 +32,7 @@ fn main() {
     let mut frontend = matches.value_of("frontend").unwrap().split(':');
 
     let out = Path::new(matches.value_of("out").unwrap());
-    let _articles = matches.value_of("articles").unwrap();
+    let articles = Path::new(matches.value_of("articles").unwrap());
 
     let repo = frontend.next().unwrap();
     let branch = if let Some(b) = frontend.next() {
@@ -46,8 +46,35 @@ fn main() {
 
     if let Err(e) = git::clone_to(repo, branch, out) {
         error!("Fail to clone repo: {}", e);
+        exit(1);
     }
 
-    // 2. Parse the articles
+    // 2. Start a new thread for routes building
+    let out_clone = out.to_owned();
+    let articles_clone = articles.to_owned();
+    let routes_thread = std::thread::spawn(move || {
+        // 1. Find `routes.toml` in: `articles` / `out`
+        let mut routes_path = articles_clone.join("routes.toml");
+        if !routes_path.exists() {
+            routes_path = out_clone.join("routes.toml");
+            if !routes_path.exists() {
+                anyhow::bail!("No routes.toml found");
+            }
+        }
+        routes::build(&routes_path, &out_clone)
+    });
+
+    // === Main thread ===
+    // 1. Parse the articles
+    // TODO
+
+    // Wait for the routes thread to finish
+    if let Err(e) = routes_thread.join().unwrap() {
+        error!("Fail to build routes: {}", e);
+        exit(1);
+    }
+
+    // === Post-build ===
+    // 1. Generate sitemap.xml
     // TODO
 }
---
+++ src/routes.rs
@@ -0,0 +1,122 @@
+use serde::Deserialize;
+use std::{io::Write, path::Path};
+
+#[derive(Debug, Deserialize)]
+struct GlobalConfig {
+    title_prefix: String,
+    title_suffix: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct Lang<T> {
+    #[serde(rename = "zh-CN")]
+    zh: T,
+    #[serde(rename = "en-US")]
+    en: T,
+}
+
+#[derive(Debug, Deserialize)]
+struct RouteSubConfig {
+    title: String,
+    description: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct RouteConfig {
+    path: String,
+    robots: String,
+    #[serde(rename = "zh-CN")]
+    zh: RouteSubConfig,
+    #[serde(rename = "en-US")]
+    en: RouteSubConfig,
+}
+
+#[derive(Debug, Deserialize)]
+struct Config {
+    global: Lang<GlobalConfig>,
+    routes: Vec<RouteConfig>,
+}
+
+impl Config {
+    pub fn load(path: &Path) -> Result<Self, std::io::Error> {
+        let config = std::fs::read_to_string(path)?;
+        let config: Config = toml::from_str(&config)?;
+        Ok(config)
+    }
+    pub fn decorate_title(&self, lang: &str, title: &str) -> String {
+        let global = &self.global;
+        let prefix = if lang == "zh-CN" {
+            &global.zh.title_prefix
+        } else {
+            &global.en.title_prefix
+        };
+        let suffix = if lang == "zh-CN" {
+            &global.zh.title_suffix
+        } else {
+            &global.en.title_suffix
+        };
+        format!("{}{}{}", prefix, title, suffix)
+    }
+}
+
+pub fn minify(template: String) -> anyhow::Result<String> {
+    let template = minify_html::minify(template.as_bytes(), &minify_html::Cfg::new());
+    String::from_utf8(template).map_err(|e| anyhow::anyhow!(e))
+}
+
+pub fn build(routes_path: &Path, out: &Path) -> anyhow::Result<()> {
+    let config = Config::load(routes_path)?;
+
+    let template = std::fs::read_to_string(out.join("index.html"))?;
+    // Available handlebars: {{title}} {{description}} {{robots}} {{lang}}
+    for route in &config.routes {
+        for lang in ["zh-CN", "en-US"] {
+            let out_path = out.join(lang).join(if route.path.starts_with("/") {
+                format!(".{}", &route.path)
+            } else {
+                format!("{}", &route.path)
+            });
+            // Check if the path ends with .html
+            if !out_path.extension().eq(&Some(std::ffi::OsStr::new("html"))) {
+                anyhow::bail!("The path {} must end with .html", out_path.display());
+            }
+            std::fs::create_dir_all(out_path.parent().unwrap())?;
+            dbg!(&out_path);
+            let mut out_file = std::fs::File::create(out_path)?;
+            let temp = template
+                .replace(
+                    "{{title}}",
+                    config
+                        .decorate_title(
+                            &lang,
+                            if lang == "zh-CN" {
+                                &route.zh.title
+                            } else {
+                                &route.en.title
+                            },
+                        )
+                        .as_str(),
+                )
+                .replace(
+                    "{{description}}",
+                    if lang == "zh-CN" {
+                        &route.zh.description
+                    } else {
+                        &route.en.description
+                    },
+                )
+                .replace("{{robots}}", &route.robots)
+                .replace("{{lang}}", lang);
+            let minified = minify(temp)?;
+            out_file.write_all(minified.as_bytes())?;
+        }
+    }
+
+    Ok(())
+}
+
+#[test]
+fn test_build_routes() -> anyhow::Result<()> {
+    info!("Testing build routes");
+    build(Path::new("./articles/routes.toml"), Path::new("./dist"))
+}
